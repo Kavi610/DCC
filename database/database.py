feb2171eed593ebb52afcd9aada272bc395fd9f8
Kavi610 <138317599+Kavi610@users.noreply.github.com>
2025-02-06 01:21:16 +0530

    database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,16 @@
+import sqlite3
 
+def setup_database():
+    conn = sqlite3.connect("inventory.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS inventory (
+            name TEXT PRIMARY KEY,
+            quantity INTEGER
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setup_database()
